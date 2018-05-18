89799a4fe488f390d5c1fd5c8e11eaa081e41f4e
cbdaughety <daughetycam@gmail.com>
2018-05-17 21:46:02 -0400

    Added Jquery file and started work on requests.

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,15 @@
 
 $(function (){
 
+  var $people = $('people')
   $.ajax({
-    type: 'GET'
-  })
+    type: 'GET',
+    url: './src/routes/routes.js',
+    success: function(people) {
+      $.each(people, function(i, people) {
+        $orders.append('<li>my people</li>');
+      })
+    }
+  });
 
 });
